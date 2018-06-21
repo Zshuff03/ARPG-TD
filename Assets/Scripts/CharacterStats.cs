e8102298273a9c8f84f1e29e370b0d5d12aca0dd
Axxis05 <cshuff05@gmail.com>
2018-06-21 18:37:44 -0400

    System for Character Stats
    Built system for character attributes
    Added a basic character script

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ARPGTD.CharacterStats{
+    /// CharacterStat is a class built to handle changes in character stats
+    /// The stats themselves will be stored in the Character object
+    [Serializable]
+    public class CharacterStat
+    {
+        public float BaseValue;                             //Base value of the stat
+
+        public virtual float Value {                                //The value of the stat during final value calculation
+            get {
+                if (isDirty || BaseValue != lastBaseValue)
+                {
+                    lastBaseValue = BaseValue;
+                    _value = CalculateFinalValue();
+                    isDirty = false;
+                }
+                return CalculateFinalValue();
+            }
+        }
+
+        protected bool isDirty = true;                        //Boolean to describe if a change has been made
+        protected float _value;                               //Last known value of the stat - set in Value
+        protected float lastBaseValue = float.MinValue;       //Last known base value of the stat
+
+        protected readonly List<StatModifier> statModifiers;
+        public readonly ReadOnlyCollection<StatModifier> StatModifiers;
+
+        /// Creator
+        public CharacterStat()
+        {
+            statModifiers = new List<StatModifier>();
+            StatModifiers = statModifiers.AsReadOnly();
+        }
+
+        ///Creator
+        public CharacterStat(float baseValue) : this()
+        {
+            BaseValue = baseValue;
+        }
+
+        ///Compare the order for sorting, making the multiplication happen after addition to the stats
+        protected virtual int CompareModifierOrder(StatModifier a, StatModifier b)
+        {
+            if (a.Order < b.Order)
+                return -1;
+            else if (a.Order > b.Order)
+                return 1;
+            return 0; // if (a.Order == b.Order
+        }
+
+        ///Add to the list
+        public virtual void AddModifier(StatModifier mod)
+        {
+            isDirty = true;
+            statModifiers.Add(mod);
+            statModifiers.Sort(CompareModifierOrder);
+        }
+
+        ///Remove from the list
+        public virtual bool RemoveModifier(StatModifier mod)
+        {
+            if (statModifiers.Remove(mod))
+            {
+                isDirty = true;
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual bool RemoveAllModifiersFromSource(object source)
+        {
+            bool didRemove = false;
+
+            for (int i = statModifiers.Count - 1; i >= 0; i--)
+            {
+                if (statModifiers[i].Source == source)
+                {
+                    isDirty = true;
+                    didRemove = true;
+                    statModifiers.RemoveAt(i);
+                }
+            }
+
+            return didRemove;
+        }
+
+        ///Calculate the final value - additive values first then multiplicative
+        protected virtual float CalculateFinalValue()
+        {
+            float finalValue = BaseValue;
+            float sumPercentAdd = 0;
+
+            for (int i = 0; i < statModifiers.Count; i++)
+            {
+                StatModifier mod = statModifiers[i];
+
+                if (mod.Type == StatModType.Flat)
+                {
+                    finalValue += mod.Value;
+                }
+                else if (mod.Type == StatModType.PercentAdd) {
+                    sumPercentAdd += mod.Value;
+
+                    if (i + 1 >= statModifiers.Count || statModifiers[i + 1].Type != StatModType.PercentAdd)
+                    {
+                        finalValue *= 1 + sumPercentAdd;
+                        sumPercentAdd = 0;
+                    }
+                }
+                else if (mod.Type == StatModType.PercentMult)
+                {
+                    finalValue *= 1 + mod.Value;
+                }
+            }
+
+            return (float)Math.Round(finalValue, 4);
+        }
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using ARPGTD.CharacterStats;
+
+public class Item
+{
+    public void Equip(Character c)
+    {
+        c.Strength.AddModifier(new StatModifier(10, StatModType.Flat, this));
+        c.Strength.AddModifier(new StatModifier(0.1f, StatModType.PercentMult, this));
+    }
+
+    public void Unequip(Character c)
+    {
+        c.Strength.RemoveAllModifiersFromSource(this);
+    }
+}
