c2dc5b96f9b99dd42de92f2e59d71aca70dd6ab0
Axxis05 <cshuff05@gmail.com>
2018-06-26 22:43:54 -0400

    Made a menu
    Created menu and scripts
    Made key press for menu tab

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+protected GameObject pauseCanvas;
+	void Start(){
+		pauseCanvas = GameObject.Find("PauseCanvas");
+		pauseCanvas.SetActive(false);
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Tab)){
+			if (!pauseCanvas.activeInHierarchy){
+				Pause();
+			}
+			else if(pauseCanvas.activeInHierarchy){
+				Resume();
+			}
+		}
+	}
+
+	public void Pause(){
+		Debug.Log("Pausing game.");
+		Time.timeScale = 0;
+		pauseCanvas.SetActive(true);
+
+		//Add things to disable here
+	}
+
+	public void Resume(){
+		Debug.Log("Unpausing game.");
+		Time.timeScale = 1;
+		pauseCanvas.SetActive(false);
+
+		//Add things to resume here
+	}
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+	
+	public void quit(){
+		Debug.Log("Quitting");
+		Application.Quit();
+	}
+}
