66a2b4c3edb46cc657687db03eb221404e40f40e
Zach Shuffield <zshuff03@yahoo.com>
2018-07-08 12:02:00 -0400

    Set up randomized terrians
    This marks a stopping point, before I start normalizing all of the terrian.

--- Assets/TheGrid/EnvironmentScripts/BasicTile.cs
+++ Assets/TheGrid/EnvironmentScripts/BasicTile.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class BasicTile : MonoBehaviour {
 
 private int width = 10;
 private int widthInTiles = 1;
-private int height = 10;
-private int heightInTiles = 1;
+private int length = 10;
+private int lengthInTiles = 1;
 
 public bool buildable;
+public float rawHeight;
+public int planeLevel;
 public int xLoc;
 public int yLoc;
 
@@ -17,7 +20,36 @@ public GameObject objectContained;
 
 	void setObject(GameObject objectToBuild) {
 		objectContained = objectToBuild;
-		Vector3Int pos = new Vector3Int(xLoc - (width/2), 0, yLoc - (height/2));
+		Vector3Int pos = new Vector3Int(xLoc - (width/2), 0, yLoc - (length/2));
 		GameObject.Instantiate(objectToBuild, pos, Quaternion.identity);
+	}
+
+	public void calcTileHieght() {
+		if(rawHeight < .3f) {
+			planeLevel = 0;
+		} else if(rawHeight >= .3f && rawHeight < .6f) {
+			planeLevel = 1;
+		} else {
+			planeLevel = 2;
+		}
+		setTransformHeight();
+	}
+	
+	public void setOutterWall() {
+		resetTransform();
+		planeLevel = 4;
+		setTransformHeight();
+	}
+
+	private void resetTransform() {
+		Vector3 currentLocal = transform.localScale;
+		transform.localScale = new Vector3(currentLocal.x , 1, currentLocal.z);
+	}
+
+	void setTransformHeight() {
+		Vector3 newScale = transform.localScale;
+		newScale.y = planeLevel * 4 + newScale.y;
+		transform.localScale = newScale;
+		transform.position = new Vector3(transform.position.x, newScale.y / 2, transform.position.z);
 	}
 }
