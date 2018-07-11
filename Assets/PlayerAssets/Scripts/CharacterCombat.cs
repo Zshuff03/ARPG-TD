a5cedf2ecec43769ac86422cf7a745c32922e010
Axxis05 <cshuff05@gmail.com>
2018-07-10 23:08:25 -0400

    Combat and enemies
    Made some enemies
    Made enemies slap player
    Made player slap enemies

---
+++ Assets/PlayerAssets/Scripts/CharacterCombat.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+public class CharacterCombat : MonoBehaviour {
+
+	CharacterStats myStats;
+
+	public float attackSpeed = 1f;								//Attack speed of the character - affected by char stats eventually
+	public float attackCooldown = 0f;							//Cooldown of basic attack. Starts at 0 because attack isn't cd
+
+	public float attackDelay = .6f;								//Delay for the attack to match up with animations
+
+	public event System.Action OnAttack;						//Callback Method for attacks
+
+	void Start() {
+		//Get stats for this character
+		myStats = GetComponent<CharacterStats>();
+	}
+
+	void Update() {
+		//Reduce CD
+		attackCooldown -= Time.deltaTime;
+	}
+
+	//Attack that motherlicker
+	public void Attack(CharacterStats targetStats) {
+		if(attackCooldown <= 0f){
+			StartCoroutine(DoDamage(targetStats, attackDelay));
+
+			if (OnAttack != null){
+				attackCooldown = 1f / attackSpeed;
+			}
+		}
+	}
+
+	//Coroutine to delay attack based on animation
+	IEnumerator DoDamage(CharacterStats stats, float delay) {
+		yield return new WaitForSeconds(delay);
+
+		stats.TakeDamage(myStats.damage.CalcValue());
+	}
+}
---
+++ Assets/PlayerAssets/Scripts/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	#region Singleton
+
+	public static PlayerManager instance;					//The PlayerManager singleton
+
+	void Awake() {
+		instance = this;
+	}
+
+	#endregion
+
+	public GameObject player;								//The player GameObejct
+
+	//Make player do things on death
+	public void KillPlayer(){
+		Debug.Log("Player died. RIP NERD");
+
+		//respawn in town after blah blah blah days
+	}
+}
